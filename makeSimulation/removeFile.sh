e5c677ff7052c1d1ebc575a65c1b2bc565b7d055
Alberto <93335022+albertos-2002@users.noreply.github.com>
2026-07-26 00:01:37 +0200

    Create removeFile.sh

---
+++ makeSimulation/removeFile.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+rm -vi ./outFile/md_gap+zbl_nve.log 
+rm -vi ./outFile/md_gap+zbl_nve.traj
+rm -vi ./outFile/temperatura-energia.csv
+rm -vi ./outFile/distanze.csv
+rm -vi ./outFile/forze_tot.csv
+rm -vi ./outFile/energ_gap+zbl.csv
+rm -vi ./outFile/forze_gap+zbl.csv
+rm -rvi ./outGraph/immagini_MD_NVE_2D
+rm -rvi ./outGraph/immagini_MD_NVE_3D
+rm -rvi ./outGraph/andamento_energia_nve.png
+rm -rvi ./outGraph/andamento_forze_totali_nve.png
+rm -rvi ./outGraph/andamento_forze_totali_nve-blocchi.png

7da11be7b5bc5f6657dadef2860a0fc99376b337
Alberto <93335022+albertos-2002@users.noreply.github.com>
2026-07-26 00:01:31 +0200

    Create sim.py

---
+++ makeSimulation/sim.py
@@ -0,0 +1,27 @@
+from functionFile import setConfig
+from functionFile import classSimulation
+from functionFile import classCalcDataFile
+from functionFile import classGraph
+
+
+def main():
+
+    print("Setting dei parametri...")
+    setConfig.carica_parametri_globali("param.config")
+
+    print("Run della simulazione...")
+#    sim = classSimulation.class_makeSimulation()
+#    sim.runSimulation()
+
+    print("Caricamento dati...")
+#    cdf = classCalcDataFile.class_calcDataFile()
+#    cdf.calcData()
+
+    print("Generazione dei grafici finali...")
+    graph = classGraph.class_makeGraph()
+    graph.makeGraph()
+
+    print("Tutto completato! Simulazione eseguita ed i grafici sono stati generati con successo.")
+    
+if __name__ == "__main__":
+    main()

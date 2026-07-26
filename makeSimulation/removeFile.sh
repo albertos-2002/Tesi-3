1124692ca7fe460a29c81b785d1d205c72d52b76
Alberto <93335022+albertos-2002@users.noreply.github.com>
2026-07-26 11:49:42 +0200

    Update removeFile.sh

--- makeSimulation/removeFile.sh
+++ makeSimulation/removeFile.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 
-rm -vi ./outFile/md_gap+zbl_nve.log 
-rm -vi ./outFile/md_gap+zbl_nve.traj
-rm -vi ./outFile/temperatura-energia.csv
-rm -vi ./outFile/distanze.csv
-rm -vi ./outFile/forze_tot.csv
-rm -vi ./outFile/energ_gap+zbl.csv
-rm -vi ./outFile/forze_gap+zbl.csv
-rm -rvi ./outGraph/immagini_MD_NVE_2D
-rm -rvi ./outGraph/immagini_MD_NVE_3D
-rm -rvi ./outGraph/andamento_energia_nve.png
-rm -rvi ./outGraph/andamento_forze_totali_nve.png
-rm -rvi ./outGraph/andamento_forze_totali_nve-blocchi.png
+rm -v ./outFile/md_gap+zbl_nve.log 
+rm -v ./outFile/md_gap+zbl_nve.traj
+rm -v ./outFile/temperatura-energia.csv
+rm -v ./outFile/distanze.csv
+rm -v ./outFile/forze_tot.csv
+rm -v ./outFile/energ_gap+zbl.csv
+rm -v ./outFile/forze_gap+zbl.csv
+rm -rv ./outGraph/immagini_MD_NVE_2D
+rm -rv ./outGraph/immagini_MD_NVE_3D
+rm -rv ./outGraph/andamento_energia_nve.png
+rm -rv ./outGraph/andamento_forze_totali_nve.png
+rm -rv ./outGraph/andamento_forze_totali_nve-blocchi.png
+rm -v ./outFile/velocita.csv
+rm -v ./outFile/forze_proiettate_radiali.csv
+rm -v ./outGraph/andamento_forze_totali_distanza3d_nve.png
+rm -v ./outGraph/andamento_forze_totali_distanza_nve.png
+rm -v ./outGraph/confronto_forze_vs_distanza_minima.png
+rm -v ./outGraph/velocita_vs_tempo.png
+rm -v ./outGraph/confronto_forze_vs_tempo.png
+rm -v ./outGraph/histo_distanze.png
+rm -v ./outGraph/Proiezione_radiale_forze.png
+rm -v ./outGraph/velocita_vs_distanza_minima.png
+rm -v ./outGraph/switch_zbl.png
